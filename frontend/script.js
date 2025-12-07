11353d5d803820d88d37e3fc7224c1faa4b6e7c8
Namuco <karlcedrick.namuco@tup.edu.ph>
2025-12-07 19:00:20 +0800

    small fix

--- frontend/script.js
+++ frontend/script.js
@@ -118,7 +118,7 @@ function setupMissionVision() {
         missionBtn.addEventListener("click", function(event) {
             event.preventDefault();
             const mission = "Mission: To help travelers by providing a secure, easy, and comprehensive platform that simplifies the tiring process of trip planning and ensures a smooth, enjoyable, and stress-free travel experience. To promote hidden tourist destinations, encouraging travelers to discover new places and experience its beauty.";
-            const vision = "Vision: To be a reliable and innovative travel website...";
+            const vision = "Vision: To be a reliable and innovative travel website that connects travelers to meaningful experiences and unforgettable journeys. In the future, KNI Travels aims to become one of the most trusted travel services.";
             alert(mission + "\n\n" + vision);
         });
     }
